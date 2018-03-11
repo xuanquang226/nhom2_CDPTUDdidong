4456ec5cfef8a6048d6561d87b475a75c6fbb858
xuanquang226 <buixuanquang2206@gmail.com>
2018-03-11 13:22:37 +0700

    layout,activity Quang

---
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/EnteringDrugActivity.java
@@ -0,0 +1,13 @@
+package com.example.quang.project_sdo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnteringDrugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entering_drug_layout);
+    }
+}
---
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/SalesStatisticsActivity.java
@@ -0,0 +1,13 @@
+package com.example.quang.project_sdo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalesStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sales_statistics_layout);
+    }
+}
