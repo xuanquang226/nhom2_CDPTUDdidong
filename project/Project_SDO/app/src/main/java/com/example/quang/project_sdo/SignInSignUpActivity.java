cfdb2ff5bd3479bd665d6dad9ef3625076b0d9ef
An <truonganit96@gmail.com>
2018-04-03 11:06:26 +0700

    layout signin signup

---
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/SignInSignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.quang.project_sdo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_signup_layout);
+    }
+}
