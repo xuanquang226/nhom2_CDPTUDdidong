72b91bd6f8db905be734d4801191e2bc5104d1cf
kupivampire <kupivampire@gmail.com>
2018-04-05 16:32:27 +0700

    them code trong phần menu search

--- project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/BackStackActivity.java
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/BackStackActivity.java
@@ -6,10 +6,14 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
+import android.widget.SearchView.OnQueryTextListener;
 
 public class BackStackActivity extends AppCompatActivity {
     ImageButton imgHome;
@@ -71,7 +75,25 @@ public class BackStackActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // dua nut search vao action bar
         getMenuInflater().inflate(R.menu.menu_search_home,menu);
+       // tao 1 search view
+        android.support.v7.widget.SearchView searchView = (android.support.v7.widget.SearchView) menu.findItem(R.id.menuSearchHome).getActionView();
+        //bat su kien cho nut search
+
+       searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
+           @Override
+           public boolean onQueryTextSubmit(String query) {
+               Toast.makeText(BackStackActivity.this, query, Toast.LENGTH_SHORT).show();
+               return false;
+           }
+
+           @Override
+           public boolean onQueryTextChange(String newText) {
+               Log.d("ABC", newText);
+               return false;
+           }
+       });
         return super.onCreateOptionsMenu(menu);
     }
 }
