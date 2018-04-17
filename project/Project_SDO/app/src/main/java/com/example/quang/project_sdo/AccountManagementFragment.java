bd13b7893d634f2af5e0c87423fad9f9564648cd
xuanquang226 <buixuanquang2206@gmail.com>
2018-04-17 08:23:24 +0700

    Sự kiện cho account

--- project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/AccountManagementFragment.java
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/AccountManagementFragment.java
@@ -3,12 +3,14 @@ package com.example.quang.project_sdo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -18,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountManagementFragment extends Fragment {
     FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthLis;
 
     @Nullable
     @Override
@@ -58,7 +61,29 @@ public class AccountManagementFragment extends Fragment {
 
             }
         });
+
+        mAuthLis = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (mAuth.getCurrentUser() == null) {
+                    Toast.makeText(getActivity(),"Đăng nhập để thực hiện chức năng này",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getActivity(), SignInSignUpActivity.class));
+                }
+            }
+        };
+
+
         return view;
     }
     //aaa
+
+    public void signOut(){
+        signOut();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthLis);
+    }
 }
