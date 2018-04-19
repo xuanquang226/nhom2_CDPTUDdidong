ab5abf53a9a6f04d046ba09a18aa9c46b4fbd7ee
kupivampire <kupivampire@gmail.com>
2018-04-19 13:17:14 +0700

    Chỉnh sửa giao diện sigup + add các fragment account

---
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/ShipperSignUpFragment.java
@@ -0,0 +1,29 @@
+package com.example.quang.project_sdo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShipperSignUpFragment extends Fragment {
+
+
+    public ShipperSignUpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frm_shipper_sign_up_layout, container, false);
+    }
+
+}
--- project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/SignUpActivity.java
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/SignUpActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -21,6 +23,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -29,8 +33,10 @@ public class SignUpActivity extends AppCompatActivity {
     EditText edtRePass;
     EditText edtAddress;
     EditText edtPhone;
+    private ArrayList<String> arrayListSpinner;
     //private ArrayList<mdSpinnerPlace> arrSpinnerType;
-
+    // Khoi tao cai vi tri
+    int possition;
     private FirebaseAuth mAuth;
     private ProgressDialog mProgress;
     private DatabaseReference root;
@@ -50,6 +56,52 @@ public class SignUpActivity extends AppCompatActivity {
         final EditText edtAddress = (EditText) findViewById(R.id.edtAddress);
         final EditText edtPhone = (EditText) findViewById(R.id.edtPhone);
         Spinner spinner = (Spinner) findViewById(R.id.spinnerChoiceAccount);
+        arrayListSpinner = new ArrayList<String>();
+        arrayListSpinner.add("User");
+        arrayListSpinner.add("Seller");
+        arrayListSpinner.add("Shipper");
+        arrayListSpinner.add("Carriers");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,arrayListSpinner);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                possition = i;
+                if (possition == 0) {
+                    UserSignUpFragment fragment1 = new UserSignUpFragment(); // Gọi Fragment cần hiểu thị với tên fragment1
+                    // Tạo ra fragmentTransaction
+                    android.support.v4.app.FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    // Replace nó :
+                    // NOTE : R.id.flScreen với flScreen là tên id của của FrameLayout bên layout sign_ip_layout
+                    fragmentTransaction1.replace(R.id.flScreen,fragment1,"Fragment");
+                    fragmentTransaction1.commit();
+                }else if (possition == 1) {
+                    SellerSignUpFragment fragment2 = new SellerSignUpFragment();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.flScreen,fragment2,"Fragment");
+                    fragmentTransaction2.commit();
+                }else if (possition == 2) {
+                    ShipperSignUpFragment fragment3 = new ShipperSignUpFragment();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.flScreen,fragment3,"Fragment");
+                    fragmentTransaction3.commit();
+                }else if (possition == 3) {
+                    CarriersSignUpFragment fragment4 = new CarriersSignUpFragment();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction4.replace(R.id.flScreen,fragment4,"Fragment");
+                    fragmentTransaction4.commit();
+                }
+        }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
         root = FirebaseDatabase.getInstance().getReference("Users Info");
@@ -110,5 +162,4 @@ public class SignUpActivity extends AppCompatActivity {
         UsersModel model = new UsersModel(email,password,address,phone);
         current_user_id.setValue(model);
     }
-
 }
