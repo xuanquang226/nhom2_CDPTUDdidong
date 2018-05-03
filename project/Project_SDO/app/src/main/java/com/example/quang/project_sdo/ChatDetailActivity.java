dea2c822ef819a2c54591a51fe1606bac40e41bc
xuanquang226 <buixuanquang2206@gmail.com>
2018-05-03 14:51:25 +0700

    z

--- project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/ChatDetailActivity.java
+++ project/Project_SDO/app/src/main/java/com/example/quang/project_sdo/ChatDetailActivity.java
@@ -158,7 +158,7 @@ public class ChatDetailActivity extends AppCompatActivity {
 
 
                 if (model.isSender()) {
-                    // set visibility
+                    // set visibilityll
                     imgReceiver.setVisibility(View.INVISIBLE);
                     imgSender.setVisibility(View.VISIBLE);
 
